4ef76265ef11576e5f823438501136727b9b2747
unknown <sway11466@gmail.com>
2023-04-15 15:21:08 +0900

    add dummy home. for test.

---
+++ src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page>
+    <q-list>
+      <q-item>
+        <q-item-section side>
+          <q-btn square icon="star" @click="test"/>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useSettingsStore } from 'src/stores/Settings.js';
+
+function useTest() {
+  async function test() {
+    const hoge = await window.native.test();
+    console.log(hoge);
+    // await window.native.saveConfig({a: 'yes', b: 'no'});
+    // const message = await window.native.loadConfig();
+    // console.log(hoge)
+
+  }
+  return { test };
+}
+
+export default defineComponent({
+  name: 'ConnectsPage',
+  components: { },
+  setup () {
+    const settings = useSettingsStore();
+    return { ...useTest(), settings };
+  }
+});
+</script>
