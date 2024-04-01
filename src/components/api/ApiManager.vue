0019f2b389b3cbacba429a3a8e113809e0d9975e
sway11466@gmail.com <sway11466@gmail.com>
2024-04-01 22:20:49 +0900

    Create ApiManager Component form HomePage.

---
+++ src/components/api/ApiManager.vue
@@ -0,0 +1,81 @@
+<template>
+  <template />
+</template>
+
+<script setup lang="ts">
+// --------------------------------
+//  supress window function error
+// --------------------------------
+export declare var window: Window;
+export interface Window {
+  shellApi: ShellApi;
+  localStorageApi: LocalStorageApi;
+  configApi: ConfigApi;
+  connectApi: ConnectApi;
+  settingApi: SettingApi;
+}
+
+// --------------------------------
+//  Init ApiManager
+// --------------------------------
+import { useApiManager } from 'src/stores/ApiManager'
+const apiManager = useApiManager();
+
+import { ShellApi } from 'src-electron/modules/shell-api';
+if (!apiManager.shellApi) { apiManager.bindShellApi(window.shellApi); }
+
+import { LocalStorageApi } from 'src-electron/modules/ls-api';
+if (!apiManager.localStorageApi) { apiManager.bindLocalStorageApi(window.localStorageApi); }
+
+import { ConfigApi } from 'src-electron/modules/config-api';
+if (!apiManager.configApi) { apiManager.bindConfigApi(window.configApi); }
+
+import { ConnectApi } from 'src-electron/modules/connects/connect-api';
+if (!apiManager.connectApi) { apiManager.bindConnectApi(window.connectApi); }
+
+import { SettingApi } from 'src-electron/modules/settings/setting-api';
+if (!apiManager.settingApi) { apiManager.bindSettingApi(window.settingApi); }
+
+// --------------------------------
+//  Books Store
+// --------------------------------
+import { useBooksStore } from 'src/stores/Books';
+const books = useBooksStore();
+
+// --------------------------------
+//  Shelf Store
+// --------------------------------
+import { useShelvesStore } from 'src/stores/Shelves';
+const shelves = useShelvesStore();
+
+// --------------------------------
+//  Labels Store
+// --------------------------------
+import { useLabelsStore } from 'src/stores/Labels';
+const labels = useLabelsStore();
+
+// --------------------------------
+//  Connectors Store
+// --------------------------------
+import { useConnectsStore } from 'src/stores/Connects';
+const connects = useConnectsStore();
+
+// --------------------------------
+//  Setting Store
+// --------------------------------
+import { useSettingsStore } from 'src/stores/Settings';
+const settings = useSettingsStore();
+
+// --------------------------------
+//  init
+// --------------------------------
+const init = async () => {
+  await settings.init();
+  await connects.init();
+  await books.init();
+  await shelves.init();
+  await labels.init();
+};
+init();
+
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,4 +1,5 @@
 <template>
+  <ApiManager />
   <q-page>
     <q-list>
       <q-item-label class="q-pb-none" header>Books</q-item-label>
@@ -10,79 +11,6 @@
 </template>
 
 <script setup lang="ts">
-//
-</script>
-
-<script lang="ts">
+import ApiManager from 'src/components/api/ApiManager.vue';
 import BookSlide from 'src/components/home/BookSlide.vue';
-import { ShellApi } from 'src-electron/modules/shell-api';
-import { LocalStorageApi } from 'src-electron/modules/ls-api';
-import { ConfigApi } from 'src-electron/modules/config-api';
-import { ConnectApi } from 'src-electron/modules/connects/connect-api';
-import { SettingApi } from 'src-electron/modules/settings/setting-api';
-
-// --------------------------------
-//  supress window function error
-// --------------------------------
-export declare var window: Window;
-export interface Window {
-  shellApi: ShellApi;
-  localStorageApi: LocalStorageApi;
-  configApi: ConfigApi;
-  connectApi: ConnectApi;
-  settingApi: SettingApi;
-}
-
-// --------------------------------
-//  ApiManager
-// --------------------------------
-import { useApiManager } from 'src/stores/ApiManager';
-const apiManager = useApiManager();
-
-// --------------------------------
-//  Books Store
-// --------------------------------
-import { useBooksStore } from 'src/stores/Books';
-const books = useBooksStore();
-
-// --------------------------------
-//  Shelf Store
-// --------------------------------
-import { useShelvesStore } from 'src/stores/Shelves';
-const shelves = useShelvesStore();
-
-// --------------------------------
-//  Labels Store
-// --------------------------------
-import { useLabelsStore } from 'src/stores/Labels';
-const labels = useLabelsStore();
-
-// --------------------------------
-//  Connectors Store
-// --------------------------------
-import { useConnectsStore } from 'src/stores/Connects';
-const connects = useConnectsStore();
-
-// --------------------------------
-//  Setting Store
-// --------------------------------
-import { useSettingsStore } from 'src/stores/Settings';
-const settings = useSettingsStore();
-
-// --------------------------------
-//  init
-// --------------------------------
-const init = async () => {
-  apiManager.bindShellApi(window.shellApi);
-  apiManager.bindLocalStorageApi(window.localStorageApi);
-  apiManager.bindConfigApi(window.configApi);
-  apiManager.bindConnectApi(window.connectApi);
-  apiManager.bindSettingApi(window.settingApi);
-  await settings.init();
-  await connects.init();
-  await books.init();
-  await shelves.init();
-  await labels.init();
-};
-init();
 </script>
