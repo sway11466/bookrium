b122e5660d1ea984aeba8e4b21c24676d79c3df5
sway11466@gmail.com <sway11466@gmail.com>
2024-04-05 22:58:31 +0900

    refactor non use logic.

--- src/components/books/BookCollections.vue
+++ src/components/books/BookCollections.vue
@@ -4,17 +4,15 @@
       <q-spinner color="primary" size="40px" />
     </template>
     <template v-for="(book, index) in books" :key="index">
-      <BookCard :book="book" @show="show" @edit="edit" />
+      <BookCard :book="book" />
     </template>
   </q-infinite-scroll>
 </template>
 
 <script setup lang="ts">
 import { PropType, Ref, ref } from 'vue';
-import { useRouter } from 'vue-router'
 import BookCard from 'src/components/books/BookCard.vue';
-import { useBooksStore } from 'src/stores/Books.js';
-import { Book, BookTypeDef } from 'src/stores/BookTypes';
+import { Book } from 'src/stores/BookTypes';
 
 const props = defineProps({
   books: {
@@ -23,22 +21,11 @@ const props = defineProps({
   },
 });
 
-const store = useBooksStore();
-const router = useRouter();
-
 const books: Ref<Book[]> = ref([] as Book[]);
 function next(index: number, done: (stop?: boolean) => void) {
   const items = props.books.slice((index-1)*10, index*10);
   books.value.push(...items);
   done(items.length === 0);
-}
-
-function show(book: BookTypeDef) {
-  store.show(book);
-}
-
-function edit(book: BookTypeDef) {
-  router.push({ path:`/books/${book.id}` });
 }
 </script>
 src/components/books/BookCollectionsOption
